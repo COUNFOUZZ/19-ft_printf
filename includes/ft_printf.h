df10896e69ee99641522be81ef1d0b88ba0d8074
Alexis Abda <aabda@student.s19.be>
2022-08-01 18:44:02 +0200

    remove useless function

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 14:06:36 by aabda             #+#    #+#             */
-/*   Updated: 2022/08/01 18:07:47 by aabda            ###   ########.fr       */
+/*   Updated: 2022/08/01 18:43:43 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int		ft_is_string(const char *format, va_list arg);
 int		ft_is_char(const char *format, va_list arg);
 int		ft_search_tc(const char *format, va_list arg);
 size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
