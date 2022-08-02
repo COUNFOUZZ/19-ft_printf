903186284269628946aead2213fa971e1aa6bf38
Alexis Abda <aabda@student.s19.be>
2022-08-02 18:40:16 +0200

    fix some prototype + add new prototype

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 14:06:36 by aabda             #+#    #+#             */
-/*   Updated: 2022/08/01 18:43:43 by aabda            ###   ########.fr       */
+/*   Updated: 2022/08/02 18:24:16 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@
 # include <stdarg.h>
 
 int		ft_printf(const char *format, ...);
-int		ft_is_string(const char *format, va_list arg);
-int		ft_is_char(const char *format, va_list arg);
+int		ft_is_string(va_list arg);
+int		ft_is_char(va_list arg);
+int		ft_is_int(va_list arg);
+int		ft_count_int(int nb);
 int		ft_search_tc(const char *format, va_list arg);
 size_t	ft_strlen(const char *s);
 void	ft_putchar_fd(char c, int fd);
