40d72ed031894ec4036d24b761e772f3e52354a1
Alexis Abda <aabda@student.s19.be>
2022-08-02 21:18:48 +0200

    add ft_is_percent.c

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 14:06:36 by aabda             #+#    #+#             */
-/*   Updated: 2022/08/02 18:24:16 by aabda            ###   ########.fr       */
+/*   Updated: 2022/08/02 20:47:40 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_is_string(va_list arg);
 int		ft_is_char(va_list arg);
 int		ft_is_int(va_list arg);
 int		ft_count_int(int nb);
+int		ft_is_percent(va_list arg);
 int		ft_search_tc(const char *format, va_list arg);
 size_t	ft_strlen(const char *s);
 void	ft_putchar_fd(char c, int fd);
