032f427ad769fb5a6ac7ddea56bc9293ff8a354e
Alexis Abda <aabda@student.s19.be>
2022-08-03 20:42:57 +0200

    add prototype for unsigned

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 14:06:36 by aabda             #+#    #+#             */
-/*   Updated: 2022/08/03 19:53:29 by aabda            ###   ########.fr       */
+/*   Updated: 2022/08/03 20:41:01 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,18 @@ int		ft_printf(const char *format, ...);
 int		ft_is_string(va_list arg);
 int		ft_is_char(va_list arg);
 int		ft_is_int(va_list arg);
+int		ft_is_unsigned(va_list arg);
 int		ft_is_hex(const char format, va_list arg);
 int		ft_is_percent(va_list arg);
 int		ft_is_pointer(va_list arg);
 int		ft_count_int(int nb);
+int		ft_count_unsigned(unsigned int nb);
 int		ft_count_hex(int nb);
 int		ft_search_tc(const char *format, va_list arg);
 size_t	ft_strlen(const char *s);
 void	ft_putnbr_hex_fd(const char format, unsigned int n, int fd);
 void	ft_putnbr_hex_pointer_fd(unsigned long n, int fd);
+void	ft_putnbr_unsigned_fd(unsigned int n, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
