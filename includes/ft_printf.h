e35e78252f2d57a0aab1d28d1996ebd281bbfda5
Alexis Abda <aabda@student.s19.be>
2022-08-03 19:48:23 +0200

    add ft_is_pointer.c

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 14:06:36 by aabda             #+#    #+#             */
-/*   Updated: 2022/08/03 18:27:44 by aabda            ###   ########.fr       */
+/*   Updated: 2022/08/03 19:46:27 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int		ft_is_char(va_list arg);
 int		ft_is_int(va_list arg);
 int		ft_is_hex(const char format, va_list arg);
 int		ft_is_percent(va_list arg);
+int		ft_is_pointer(va_list arg);
 int		ft_count_int(int nb);
 int		ft_count_hex(int nb);
 int		ft_search_tc(const char *format, va_list arg);
---
+++ srcs/ft_is_pointer.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_pointer.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 19:33:57 by aabda             #+#    #+#             */
+/*   Updated: 2022/08/03 19:43:52 by aabda            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+int	ft_is_pointer(va_list arg)
+{
+	unsigned int	nb;
+	int				count;
+
+	if (!arg)
+		return (0);
+	nb = va_arg(arg, unsigned long);
+	count = ft_count_hex(nb);
+	ft_putstr_fd("0x", 1);
+	ft_putnbr_hex_fd('x', nb, 1);
+	return (count + 2);
+}
