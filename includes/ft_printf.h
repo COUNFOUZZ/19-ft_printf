449ab65a61f77be50aad78ca688d1dc490d249e5
Alexis Abda <aabda@student.s19.be>
2022-08-03 19:53:54 +0200

    add prototype for putnbr hex for pointer

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 14:06:36 by aabda             #+#    #+#             */
-/*   Updated: 2022/08/03 19:46:27 by aabda            ###   ########.fr       */
+/*   Updated: 2022/08/03 19:53:29 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int		ft_count_hex(int nb);
 int		ft_search_tc(const char *format, va_list arg);
 size_t	ft_strlen(const char *s);
 void	ft_putnbr_hex_fd(const char format, unsigned int n, int fd);
+void	ft_putnbr_hex_pointer_fd(unsigned long n, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
