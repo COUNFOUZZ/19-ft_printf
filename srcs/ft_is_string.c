9feeefaac728bd0e6d2267a77e33bb6d2804eeb3
Alexis Abda <aabda@student.s19.be>
2022-08-04 11:40:35 +0200

    arg of %s is NULL printf -> (null) and return (6)

--- srcs/ft_is_string.c
+++ srcs/ft_is_string.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:50:12 by aabda             #+#    #+#             */
-/*   Updated: 2022/08/03 19:44:23 by aabda            ###   ########.fr       */
+/*   Updated: 2022/08/04 11:39:05 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ int	ft_is_string(va_list arg)
 	if (!arg)
 		return (0);
 	s = va_arg(arg, char *);
+	if (!s)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
 	ft_putstr_fd(s, 1);
 	return (ft_strlen(s));
 }
